cab6bc86b2a3520e198c315cb6801c3da8c004ff
Robby Evans <ruttoevans001@gmail.com>
2025-10-04 17:18:39 +0300

    minor fix

--- server/app/models/application.rb
+++ server/app/models/application.rb
@@ -3,7 +3,7 @@ belongs_to :user
 belongs_to :athlete
 has_many :documents, dependent: :destroy
 
-enum status: { pending: 0, approved: 1, rejected: 2, invoiced: 3 }
+enum :status, { pending: 0, approved: 1, rejected: 2, invoiced: 3 }
 
 validates :country, presence: true
 end
--- server/app/models/user.rb
+++ server/app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   has_secure_password
   
-  enum role: { individual: 0, corporate: 1, admin: 2 }
+  enum :role, { individual: 0, corporate: 1, admin: 2 }
   
   has_many :athletes, dependent: :destroy
   has_many :applications, dependent: :destroy
