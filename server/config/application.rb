36cbd9a9f31a1ff5dcdc27c6f4a8043ea5a438eb
Robby Evans <ruttoevans001@gmail.com>
2025-11-18 22:00:51 +0300

    minor fix

--- server/config/application.rb
+++ server/config/application.rb
@@ -8,26 +8,28 @@ module Visapro
     config.autoload_lib(ignore: %w[assets tasks])
     config.api_only = true
 
-    # CORS
+    # ===============================
+    # CORS CONFIGURATION (GLOBAL)
+    # ===============================
     config.middleware.insert_before 0, Rack::Cors do
       allow do
-      origins_allowed =
-        (ENV['ALLOWED_ORIGINS']&.split(',')&.map(&:strip) || [
-          'http://localhost:5173',
-          'http://127.0.0.1:5173',
-          'http://192.168.2.129:5173',
-          'https://visapro-dusky.vercel.app'
-        ])
+        origins_allowed =
+          (ENV['ALLOWED_ORIGINS']&.split(',')&.map(&:strip) || [
+            "http://localhost:5173",
+            "http://127.0.0.1:5173",
+            "https://visapro-dusky.vercel.app"
+          ])
 
         origins(*origins_allowed)
 
-        resource '*',
+        resource "*",
           headers: :any,
           methods: [:get, :post, :put, :patch, :delete, :options, :head],
           credentials: false,
           max_age: 600
+      end
     end
-  end
+    # ===============================
 
   end
 end
