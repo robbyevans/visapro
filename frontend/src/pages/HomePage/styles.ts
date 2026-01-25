0c6f3dc1bc3a5dcd05051b79487af9a7cde32a09
SirmaCaleb <Calebsirma02@gmail.com>
2026-01-25 16:43:27 -0500

    fix: correct hero images

--- frontend/src/pages/HomePage/styles.ts
+++ frontend/src/pages/HomePage/styles.ts
@@ -1,7 +1,7 @@
 import styled from "styled-components";
-import heroDesktop from "../../assets/visapro-desktop-background.webp";
-import heroTablet from "../../assets/visapro-tablet-background.webp";
-import heroMobile from "../../assets/visapro-mobile-background.webp";
+import heroDesktop from "../../assets/vis-desktop.webp";
+import heroTablet from "../../assets/vis-tablet.webp";
+import heroMobile from "../../assets/vis-mobile.webp";
 import { STATIC_COLORS } from "../../styles";
 
 export const HomePageContainer = styled.div`
